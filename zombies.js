cefbf0e73db45627e4c11d4097390392493263ba
caitypizza <caitykondo@gmail.com>
2016-12-03 14:08:43 -1000

    created Item class

--- zombies.js
+++ zombies.js
@@ -7,7 +7,11 @@
  * @param {string} name     The item's name.
  * @property {string} name
  */
-
+class Item {
+  constructor(name){
+    this.name = name;
+  }
+}
 
 /**
  * Class => Weapon(name, damage)
